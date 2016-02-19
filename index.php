76ca0d67cdd50ce5f49d9c520f6337a4d6b066dc
IRA5 <ira5@njit.edu>
2016-02-18 17:00:58 -0800

    Install New Template

--- index.php
+++ index.php
@@ -1,20 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
 Irtaza Ali	
 100 Prospect St Jersey City, NJ 
 ira5@njit.edu
 
-Summary
+Summary:
 	
-Interest:
-I would want to work in a company that would allow me to use my computer science skills
+	Would want to work in a company that would allow me to use my computer science skills
 Computer Skills
 	
-Languages I learned in computer science: 
+Languages:
 Java, C++, C sharp, C Lang, and Python
 	
 Experience
 None	
 	
-Education
-	
+Education:
 2009-2012	HS Degree, William. L. Dickinson HS
 2012-2014	Associates Degree,  Hudson County Community College
+
+</body>
+
+</html>
