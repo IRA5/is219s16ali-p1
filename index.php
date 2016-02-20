6166cb254d9faa6c21e398a1b6a5a56101af6bea
IRA5 <ira5@njit.edu>
2016-02-19 17:03:57 -0800

    Resume

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
-<!doctype html>
-<html lang=en>
-<head>
-<meta charset="utf-8" />
-<meta name="verify-v1" content="91yIG+EaOdwKuPX+R9ChHEWVXJ6wdQsq0gqD1c1sbaY="/>
-<title>Irtaza Ali, Resume- Computer Science</title>
-<style type="text/css">
+<!DOCTYPE html>
+<html lang="en">
+
+</body>
+
+</html>
+
 body{font-family:Geneva, Arial, Helvetica, sans-serif;font-size:12px;font-weight:normal;color:#000000;text-align:center;margin:0;padding:0;line-height:1.5em;}
 header, section, article{display:block;}
 header{text-align:center;position:relative;z-index:1;}
@@ -114,70 +114,223 @@ body{font-size:18px;}
 <![endif]-->
 </head>
 
+<head>
+
+	<meta charset="utf-8" />
+	<meta name="verify-v1" content="91yIG+EaOdwKuPX+R9ChHEWVXJ6wdQsq0gqD1c1sbaY="/>
+	<title>Irtaza Ali, Resume- Computer Science</title>
+	<style type="text/css">
+
+</head>
+
 <body>
-
-<div id="links">
-  <a class="formats" id="pdf" href="dshapiro.pdf"></a>
-  <!--<a class="formats" id="doc" href="dshapiro.doc"></a>-->
-  <!--<a class="formats" id="txt" href="dshapiro.txt"></a>-->
-</div>
-
-<div id="resumecontainer">
-
-<header>
-<h1>Irtaza Ali</h1>
-<p><a href="mailto:ira5@njit.edu</a></p>
-<p><a href="https://github.com/IrtazaALI1/">https://github.com/IrtazaALI1</a></p>
-
-<p><strong>Web Developer Resume &amp; Portfolio</strong></p>
-<p><em><a rel="#notice" class="proj_link notice">Notice to 3rd Party Recruiters (Please Read)</a></em></p>
-</header>
-
-<section>
-<h2>Objective</h2>
-<p>Seeking challenging opportunities in <em><strong>Computer Science / Information Technology</strong></em> within a 25 statute mile radius of Jersey City, NJ, 07307.</p>
-</section>
-
-<section>
-<h2>Summary</h2>
-<p>in front &amp; backend web development, database design, integration, 
-implementation, software development, network</p>
-</section>
-
-<section>
-<h2>Education</h2>
-<p><strong>William. L. Dickinson HS</strong>, ComputerScience</p>
-<p><strong>Hudson County Community College College</strong>, Computer Science, 3.0 GPA</p>
-
-<h5>Notice to 3rd Party Recruiters</h5>
-<p>
-Dear Unsolicited Recruiters:
-<br/><br/>
-I am happy to work with those representing legitimate companies at fair rates.  Unfortunately, there are too many unscrupulous clowns out there trying to make a quick buck that are disrespectful of my time. So I have adopted a set of non-negotiable policies for dealing with 3rd party entities.  If you are able to respect and abide by ALL of my policies then great, perhaps we can work together, and if not, PLEASE MOVE ON.
-</p>
-<ol>
-<li>I do not deal with off-shore companies under any circumstances. You will be marked as spam.</li>
-<li>I only consider opportunities local to my current address- San Francisco, SF Peninsula, or San Jose.  Send me an out of state listing. I mark you as spam.</li>
-<li>Send a DETAILED JOB DESCRIPTION in the FIRST email including the EXACT CITY where the organization is located.  I do not respond to emails where you only state in your opinion that you have a great job opportunity and want to chat further.  It&rsquo;s a waste of my time and I mark you as a spammer. </li>
-<li>The job listing better be REAL. If it is not, you are marked as spam.</li>
-<li>DO NOT claim to represent a company unless you have done previous business with them- meaning you have successfully placed other candidates there, and DO NOT claim to be representing me to any organization unless all the terms in this list are agreed to and abided by.  I will make an extra effort to notify the principle organization of your fraudulent efforts.</li>
-<li>If you send me a listing, and I respond with interest, if I don't hear back from you, I mark you as spam.</li>
-<li>If you have a &quot;fast growing start-up&quot; I am not interested unless it is for an hourly contract rate.  I have ended up working for free too many times, and I will not do it again. Again, you are marked as spam.</li>
-<li>I will however, consider being part of an initial founding start-up team if the idea is clearly viable. I perform due diligence. Be prepared to provide the necessary information.</li>
-<li>I will not provide reference contact information until interviews have been conducted with the hiring manager AND interest from both sides has been expressed.</li>
-<li>I require a phone screen with the hiring manager before I will agree to an on-site interview.</li>
-<li>I will not spend more than 5 minutes creating special presentations, filling out questionnaires or taking tests.</li>
-</ol>
-</div>
-</article>
-
-</section>
-
-<div id="body_popup">
-<a class="close">[X] Close</a>
-</div>
-
-
-</div>
+	<div class="preloader">
+		<img src="img/loader.gif" alt="Preloader image">
+	</div>
+	<nav class="navbar">
+		<div class="container">
+			<!-- Brand and toggle get grouped for better mobile display -->
+			<div class="navbar-header">
+				<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+					<span class="sr-only">Toggle navigation</span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</button>
+				<a class="navbar-brand" href="#"><img src="img/logo.png" data-active-url="img/logo-active.png" alt=""></a>
+			</div>
+			<!-- Collect the nav links, forms, and other content for toggling -->
+			<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+				<ul class="nav navbar-nav navbar-right main-nav">
+					<li><a href="#intro">Intro</a></li>
+					<li><a href="#services">Services</a></li>
+					<li><a href="#team">Team</a></li>
+					<li><a href="#pricing">Pricing</a></li>
+					<li><a href="#" data-toggle="modal" data-target="#modal1" class="btn btn-blue">Sign Up</a></li>
+				</ul>
+			</div>
+			<!-- /.navbar-collapse -->
+		</div>
+		<!-- /.container-fluid -->
+	</nav>
+	<header id="summary">
+							<h3 class="in front &amp; backend web development, database design, integration, 
+							implementation, software development, network.</h3>
+							<span class="typed-cursor">|</span>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</header>
+	<section>
+		<div class="Interest"></div>
+		
+						<h5 class="Want to protect the country against attack and want to work for the military."<h5>
+									</div>
+								</div>
+							</div>
+							<div class="Education">
+								<div class="William. L. Dickinson HS">
+								<div class="Hudson County Community College">
+								<div class="New Jersey Institute Of Technology">
+									</div>
+								</div>
+								<div class="Work Experience">
+									<div class="none">
+									</div>
+								</div>
+								<div class="Contact Information">
+									<div class="ira5@gmail.edu">
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+	<h5"Dear Unsolicited Recruiters"<h5>
+	<br/><br/>
+	I am happy to work with those representing legitimate companies at fair rates.  Unfortunately, there are too many unscrupulous clowns out there trying to make a quick buck that are disrespectful of my time. So I have adopted a set of non-negotiable policies for dealing with 3rd party entities.  If you are able to respect and abide by ALL of my policies then great, perhaps we can work together, and if not, PLEASE MOVE ON.
+	</p>
+	<ol>
+	<li>I do not deal with off-shore companies under any circumstances. You will be marked as spam.</li>
+	<li>I only consider opportunities local to my current address- San Francisco, SF Peninsula, or San Jose.  Send me an out of state listing. I mark you as spam.</li>
+	<li>Send a DETAILED JOB DESCRIPTION in the FIRST email including the EXACT CITY where the organization is located.  I do not respond to emails where you only state in your opinion that you have a great job opportunity and want to chat further.  It&rsquo;s a waste of my time and I mark you as a spammer. </li>
+	<li>The job listing better be REAL. If it is not, you are marked as spam.</li>
+	<li>DO NOT claim to represent a company unless you have done previous business with them- meaning you have successfully placed other candidates there, and DO NOT claim to be representing me to any organization unless all the terms in this list are agreed to and abided by.  I will make an extra effort to notify the principle organization of your fraudulent efforts.</li>
+	<li>If you send me a listing, and I respond with interest, if I don't hear back from you, I mark you as spam.</li>
+	<li>If you have a &quot;fast growing start-up&quot; I am not interested unless it is for an hourly contract rate.  I have ended up working for free too many times, and I will not do it again. Again, you are marked as spam.</li>
+	<li>I will however, consider being part of an initial founding start-up team if the idea is clearly viable. I perform due diligence. Be prepared to provide the necessary information.</li>
+	<li>I will not provide reference contact information until interviews have been conducted with the hiring manager AND interest from both sides has been expressed.</li>
+	<li>I require a phone screen with the hiring manager before I will agree to an on-site interview.</li>
+	<li>I will not spend more than 5 minutes creating special presentations, filling out questionnaires or taking tests.</li>
+	</ol>
+	</div>
+	</article>
+	</footer>
+	<!-- Holder for mobile navigation -->
+	<div class="mobile-nav">
+		<ul>
+		</ul>
+		<a href="#" class="close-link"><i class="arrow_up"></i></a>
+	</div>
+	<!-- Scripts -->
+	<script src="js/jquery-1.11.1.min.js"></script>
+	<script src="js/owl.carousel.min.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+	<script src="js/wow.min.js"></script>
+	<script src="js/typewriter.js"></script>
+	<script src="js/jquery.onepagenav.js"></script>
+	<script src="js/main.js"></script>
 </body>
+
 </html>
+
+body{font-family:Geneva, Arial, Helvetica, sans-serif;font-size:12px;font-weight:normal;color:#000000;text-align:center;margin:0;padding:0;line-height:1.5em;}
+header, section, article{display:block;}
+header{text-align:center;position:relative;z-index:1;}
+section{margin:20px 0 10px;padding:0;}
+p{margin-top:0;padding:0;}
+ul, ol{margin:10px 0 0;padding:0;}
+h1{margin:5px;font-size:2em;font-weight:bold;}
+h2{font-size:1.4em;font-style:italic;font-weight:bold;margin:5px 0;}
+h5{margin:20px 0 10px;font-size:1.2em;font-weight:bold;}
+a, a:link, a:visited{border:medium none;text-decoration:none;color:#009;cursor:pointer;}
+#resumecontainer{background-color:#F6F6F6;text-align:left;padding:20px;margin:20px auto;min-width:300px;max-width:600px;position:relative;}
+header p{margin:5px;}
+#links img{border:0 none;}
+.skills{margin:10px 10px;}
+.project{margin:10px 0 20px 10px;}
+section > p, section > article{margin:5px 0 0 10px;}
+#skills_section{position:relative;z-index:0;}
+#body_popup{position:absolute;left:0;top:0;z-index:1000;display:none;background-color:#FFFFFF;border:1px solid #000;padding:20px;min-width:300px;max-width:440px;text-align:left;}
+a:hover{text-decoration:none;color:#03F;}
+.close{display:block;float:right;}
+li{margin-left:5px;list-style-position:inside;list-style-type:circle;padding-left:0}
+ol > li{list-style-position:inside;list-style-type:decimal;margin-bottom:10px;margin-left:0;}
+#links{position:absolute;top:0;right:0;width:60px;}
+.formats{display:inline-block;height:16px;width:16px;background:url(img/links.png) no-repeat;}
+#doc{background-position:0 0;}
+#txt{background-position:0 -52px;}
+#pdf{background-position:0 -26px;}
+.indent{margin-left:20px;}
+@media only screen and (max-device-width: 480px) {
+body{font-size:18px;}
+#resumecontainer{padding:20px;margin:20px;max-width:1000px;}	
+#body_popup{max-width:1000px;}
+}
+</style>
+<script type="text/javascript">
+(function(){
+  function loadScript(url, callback){  //lifted from Nick Zakas
+    var script = document.createElement("script")
+    script.type = "text/javascript";
+    if (script.readyState){  //IE
+      script.onreadystatechange = function(){
+        if (script.readyState == "loaded" ||
+          script.readyState == "complete"){
+          script.onreadystatechange = null;
+          callback();
+        }
+      };
+    }else{  //Others
+      script.onload = function(){
+        callback();
+      };
+    }
+    script.src = url;
+    document.body.appendChild(script);
+  }
+  //libraries to load after document.load event
+  function afterLoad() {
+	loadScript("http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js", afterJQLoad);
+	loadScript("http://www.google-analytics.com/ga.js", googleCrap);
+  }
+  //stuff to do after jQuery is loaded
+  function afterJQLoad(){
+    var details = '',
+      coords = {left:'0px', top:'0px'},
+      close = function(){
+        details = '';
+        $("#body_popup > div.temp").remove();
+	    $("#body_popup").css({left:'0px', top:'0px'}).hide();
+      };
+    $(".details, .notice").click(function(e){  
+      var $target = $(this),
+	    coords = {left:'10px', top:e.pageY + 'px'};
+	  if(details !== ''){ 
+	    close();
+	  }
+	  details = $(this.rel).html();
+	  var id = ($target.hasClass("details")) ? "#skills_popup" : "#notice_popup";
+	  $("#body_popup").append(details).css(coords).show();
+    });
+    $(".close").click(function(){
+	  close();
+    });
+    $(".external").on('click', function(){
+      var url = this.rel;
+      window.open(url);
+      return;
+    }); 
+  }
+  function googleCrap(){
+    try {
+      var pageTracker = _gat._getTracker("UA-8130141-1");
+      pageTracker._trackPageview();
+    }catch(err){}  
+  }
+  
+  if (window.addEventListener){
+    window.addEventListener("load", afterLoad, false);
+  }else if (window.attachEvent){
+    window.attachEvent("onload", afterLoad);
+  } else {
+    window.onload = afterLoad;
+  }
+})();
+</script>
+<!--[if lt IE 9]>
+<script src="//html5shim.googlecode.com/svn/trunk/html5.js"></script>
+<![endif]-->
+</head>
