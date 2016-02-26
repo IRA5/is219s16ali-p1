25845bbde5e01fcb1db4a7715c6a6b52987af53c
IRA5 <ira5@njit.edu>
2016-02-25 17:14:09 -0800

    change

--- index.php
+++ index.php
@@ -123,12 +123,8 @@
 				<div class="col-md-4">
 					<div class="team text-center">
 						<div class="cover" style="background:url('img/team/team-cover1.jpg'); background-size:cover;">
-							<div class="overlay text-center">
-								<h3 class="white">$69.00</h3>
-								<h5 class="light light-white">1 - 5 sessions / month</h5>
-							</div>
+							
 						</div>
-						<img src="img/team/team3.jpg" alt="Team Image" class="avatar">
 						<div class="title">
 							<h4>GOOGLE</h4>
 							<h5 class="muted regular">TECH Company</h5>
