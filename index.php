3a9cb8381561433ae3004e97c57d1f85cd0db87b
IRA5 <ira5@njit.edu>
2016-02-25 17:20:38 -0800

    changes in work experience

--- index.php
+++ index.php
@@ -129,7 +129,7 @@
 							<h4>GOOGLE</h4>
 							<h5 class="muted regular">TECH Company</h5>
 						</div>
-						<button data-toggle="modal" data-target="#modal1" class="btn btn-blue-fill">Sign Up Now</button>
+						
 					</div>
 				</div>
 				<div class="col-md-4">
@@ -140,23 +140,18 @@
 							<h4>Staples</h4>
 							<h5 class="muted regular">Mobile Sales Person</h5>
 						</div>
-						<a href="#" data-toggle="modal" data-target="#modal1" class="btn btn-blue-fill ripple">Sign Up Now</a>
+						
 					</div>
 				</div>
 				<div class="col-md-4">
 					<div class="team text-center">
 						<div class="cover" style="background:url('img/team/team-cover3.jpg'); background-size:cover;">
-							<div class="overlay text-center">
-								<h3 class="white">$69.00</h3>
-								<h5 class="light light-white">1 - 5 sessions / month</h5>
-							</div>
-						</div>
-						<img src="img/team/team2.jpg" alt="Team Image" class="avatar">
+					</div>
 						<div class="title">
 							<h4>Best Buy</h4>
 							<h5 class="muted regular">Sales Person</h5>
 						</div>
-						<a href="#" data-toggle="modal" data-target="#modal1" class="btn btn-blue-fill ripple">Sign Up Now</a>
+						
 					</div>
 				</div>
 			</div>
